7885e0a7c49888c62f4c3f3013d05ce27999b307
Benjamin Dev <benjasotoivan@gmail.com>
2026-05-25 12:36:41 -0400

    Selection character

---
+++ src/const/books.js
@@ -0,0 +1,25 @@
+export const books = [
+  {
+    id: 1,
+    title: "Book 1",
+    x: 100,
+    y: 400,
+  },
+  {
+    id: 2,
+    title: "Book 2",
+    x: 250,
+    y: 300,
+  },
+  {
+    id: 3,
+    title: "Book 3",
+    x: 400,
+    y: 450,
+  },
+];
+
+export const connections = [
+  [1, 2],
+  [2, 3],
+];
