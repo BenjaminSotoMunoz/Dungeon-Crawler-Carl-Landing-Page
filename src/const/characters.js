673df7e69e19fc741508a2f24366f0a40a8fef5b
Benjamin Dev <benjasotoivan@gmail.com>
2026-05-25 13:40:29 -0400

    character selection v1

---
+++ src/const/characters.js
@@ -0,0 +1,225 @@
+export const characters = [
+  {
+    id: 0,
+    name: "Donut",
+    img: "/public/character/donut-character2.png",
+    selection_img: "/public/character-selection/donut.png",
+    description:
+      "Princess Donut is the fan-favorite, royal-acting tortoiseshell Persian cat from the hit LitRPG series Dungeon Crawler Carl by Matt Dinniman. She is sapient, speaks with a sultry yet pompous voice, and possesses a massive ego, constantly referring to herself as 'the queen' or leader of the team.",
+    crawler_info: {
+      crawler_number: "4,119",
+      crawler_id: "Princess Donut",
+      origin: "Seattle, WA",
+      job: "Cat Lady",
+    },
+    quotes: [
+      "You tried to kill Katia you fucking bitch!",
+      "DIE MOTHERFUCKERS DIE!!",
+    ],
+    physical_traits: {
+      age: "4",
+      height: "???",
+      weight: "???",
+      hair: "Black, Beige, and White",
+    },
+  },
+  {
+    id: 1,
+    name: "Carl",
+    img: "/public/character/carl.png",
+    selection_img: "/public/character-selection/carl.png",
+    description:
+      "Carl is the titular protagonist of Dungeon Crawler Carl. A former coast guardsman, Carl and his ex-girlfriend's cat are now fan-favorite crawlers on the Syndicate gameshow, Dungeon Crawler World: Earth.",
+    crawler_info: {
+      crawler_number: "4.133",
+      crawler_id: "Carl",
+      origin: "Seattle, WA",
+      job: "Marine Technician",
+    },
+    quotes: [
+      "asdYou tried to kill Katia you fucking bitch!",
+      "DIE MOTHERFUCKERS DIE!!",
+    ],
+    physical_traits: {
+      age: "27",
+      height: "6'3 (190.5cm)",
+      weight: "230 lbs (104.33 kg)",
+      hair: "Brown",
+    },
+  },
+  {
+    id: 2,
+    name: "Mordekai",
+    img: "/public/character/mordekai.png",
+    selection_img: "/public/character-selection/mordekai.png",
+    description:
+      "Mordecai is Carl and Donut's Game Guide in the dungeon. He is an indentured employee of Borant Corporation, tasked with helping the corporation run crawls. Originally an avian race called a Skyfowl, Mordecai chose Changeling as his race when he was a crawler.",
+    crawler_info: {
+      crawler_number: "N/A",
+      crawler_id: "Mordekai",
+      origin: "Skyfowl Planet",
+      job: "Game Guide",
+    },
+    quotes: [
+      "You tried to kill Katia you fucking bitch!",
+      "DIE MOTHERFUCKERS DIE!!",
+    ],
+    physical_traits: {
+      age: "???",
+      height: "???",
+      weight: "???",
+      hair: "???",
+    },
+  },
+  {
+    id: 3,
+    name: "Beatrice",
+    img: "/public/character/beatriz.png",
+    selection_img: "/public/character-selection/beatriz.png",
+    description:
+      "Beatrice begins as Carl's carefree ex-girlfriend and Donut's devoted owner, a symbol of the pre-dungeon normalcy shattered by the crawl, her absence haunting Carl amid the apocalypse. ",
+    crawler_info: {
+      crawler_number: "N/A",
+      crawler_id: "N/A",
+      origin: "Seattle, WA",
+      job: "Cat Show Person, MLM Boss Babe",
+    },
+    quotes: [
+      "You don’t even like her, Carl. Why do you care?",
+      "DIE MOTHERFUCKERS DIE!!",
+    ],
+    physical_traits: {
+      age: "???",
+      height: "???",
+      weight: "???",
+      hair: "???",
+    },
+  },
+  {
+    id: 4,
+    name: "Mongo",
+    img: "/public/character/mongo.png",
+    selection_img: "/public/character-selection/mongo.png",
+    description:
+      "Mongo is a highly intelligent, rapidly growing Velociraptor (specifically a Deinonychus-sized dinosaur) that becomes the beloved pet of Princess Donut",
+    crawler_info: {
+      crawler_number: "N/A",
+      crawler_id: "Mongo",
+      origin: "Second Floor",
+      job: "Cat Show Person, MLM Boss Babe",
+    },
+    quotes: [
+      "You don’t even like her, Carl. Why do you care?",
+      "DIE MOTHERFUCKERS DIE!!",
+    ],
+    physical_traits: {
+      age: "???",
+      height: "???",
+      weight: "???",
+      hair: "Pink",
+    },
+  },
+  {
+    id: 5,
+    name: "Imani",
+    img: "/public/character/imani.png",
+    selection_img: "/public/character-selection/imani.png",
+    description:
+      "Imani is a member of the Meadow Lark party and guildmaster of Safehome Yolanda. She is one of Carl and Donut's most trusted allies in the Dungeon.",
+    crawler_info: {
+      crawler_number: "12,329,440",
+      crawler_id: "Imani C",
+      origin: "Wenatchee, WA",
+      job: "Nurse",
+    },
+    quotes: [
+      "You don’t even like her, Carl. Why do you care?",
+      "DIE MOTHERFUCKERS DIE!!",
+    ],
+    physical_traits: {
+      age: "approx. 20",
+      height: "5'2 (152.4cm)",
+      weight: "90 lbs.",
+      hair: "Brown",
+    },
+  },
+  {
+    id: 6,
+    name: "Brandon",
+    img: "/public/character/brandon.png",
+    selection_img: "/public/character-selection/brandon.png",
+    description:
+      "Before the transformation, Brandon and his brother Chris Andrews were maintenance workers at the Meadow Lark Elder Care Facility in Wenatchee, WA.",
+    crawler_info: {
+      crawler_number: "12,330,671",
+      crawler_id: "Brandon An",
+      origin: "Wenatchee, WA",
+      job: "Maintenance",
+    },
+    quotes: [
+      "We talked about it. All of us are going to go down. We’ll take our chances on the third floor. We don’t want to risk getting separated.",
+    ],
+    physical_traits: {
+      age: "???",
+      height: "???",
+      weight: "???",
+      hair: "Brown",
+    },
+  },
+  {
+    id: 7,
+    name: "Maggie My",
+    img: "/public/character/maggie.png",
+    selection_img: "/public/character-selection/maggie.png",
+    description:
+      "Maggie My is a crawler from Earth and an ex-wife to Frank Q. After her daughter Yvette's death, she swears vengeance upon Carl and Donut and focuses her efforts on killing them rather than surviving.",
+    crawler_info: {
+      crawler_number: "#324,116",
+      crawler_id: "Maggie My",
+      origin: "Seattle, WA",
+      job: "Police Detective",
+    },
+    quotes: ["You don’t know anything. You didn’t get the whole story."],
+    physical_traits: {
+      age: "approx. 40",
+      height: "???",
+      weight: "???",
+      hair: "Brown",
+    },
+  },
+  {
+    id: 8,
+    name: "Maggie My",
+    img: "/public/character/maggie.png",
+    selection_img: "/public/character-selection/maggie.png",
+    description:
+      "Maggie My is a crawler from Earth and an ex-wife to Frank Q. After her daughter Yvette's death, she swears vengeance upon Carl and Donut and focuses her efforts on killing them rather than surviving.",
+    crawler_info: {
+      crawler_number: "#324,116",
+      crawler_id: "Maggie My",
+      origin: "Seattle, WA",
+      job: "Police Detective",
+    },
+    quotes: ["You don’t know anything. You didn’t get the whole story."],
+    physical_traits: {
+      age: "approx. 40",
+      height: "???",
+      weight: "???",
+      hair: "Brown",
+    },
+  },
+];
+
+export const crawlerInfo = [
+  { key: "crawler_number", label: "Crawler Number" },
+  { key: "crawler_id", label: "Crawler ID" },
+  { key: "origin", label: "Origin" },
+  { key: "job", label: "Job" },
+];
+
+export const physicalTraits = [
+  { key: "age", label: "Age" },
+  { key: "height", label: "Height" },
+  { key: "weight", label: "Weight" },
+  { key: "hair", label: "Hair" },
+];
