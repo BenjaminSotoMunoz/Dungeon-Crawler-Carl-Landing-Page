7ba39abac802e67191e6c5983a1609ec161248f2
Benjamin Dev <benjasotoivan@gmail.com>
2026-06-03 17:54:06 -0400

    character-selection v2 and responsive

--- src/const/books.js
+++ src/const/books.js
@@ -1,22 +1,65 @@
-export const books = [
+export const booksContet = [
   {
     id: 1,
-    title: "Book 1",
-    x: 100,
-    y: 400,
+    title: "Dungeon Crawler Carl",
+    image: "/src/assets/book.jpg",
+    imageCover: "/src/assets/book1-3d.png",
+    bookInfo: "First book, floor 1-2",
+    introduction:
+      "An alien invasion forces Carl and his cat Donut into a deadly underground game show. Using a missile launcher and charisma, they survive the first levels and become instant celebrity stars.",
+    description:
+      "Dungeon Crawler Carl is a fictional LitRPG adventure novel by Matt Dinniman, and the first installment in the Dungeon Crawler Carl book series. The novel was self-published by Matt Dinniman (as 'Dandy House') in paperback and Kindle formats in 2020, followed by an audiobook release in January 2021.",
+    ebook: "",
+    book: "",
   },
   {
     id: 2,
-    title: "Book 2",
-    x: 250,
-    y: 300,
+    title: "Carl's Doomsday Scenario",
+    image: "/src/assets/book2.jpg",
+    imageCover: "/src/assets/book2-3d.png",
+    bookInfo: "Second book, floor 3",
+    introduction:
+      "Carl and Donut conquer the castle-themed 3rd floor while dealing with corporate sponsorship plots. They protect their allies and defeat a massive, multi-headed boss to survive.",
+    description:
+      "Carl's Doomsday Scenario follows Carl, Princess Donut, Mordecai, and Mongo as they navigate the brutal 3rd floor of the dungeon—a sprawling urban nightmare known as the Over City. The duo faces severe class and quest restrictions, political plots by the show's producers, and interstellar threats.",
+    ebook: "",
+    book: "",
   },
   {
     id: 3,
-    title: "Book 3",
-    x: 400,
-    y: 450,
+    title: "The Dungeon Anarchist's Cook Book",
+    image: "/src/assets/book3.jpg",
+    imageCover: "/src/assets/book3-3d.png",
+    bookInfo: "Third book, floor 4",
+    introduction:
+      "Carl and Donut navigate the deadly 4th floor, an iron tangle train network run by killer locomotives. Carl obtains the 'Anarchist's Cookbook'.",
+    description:
+      "The Dungeon Anarchist's Cookbook (Book 3) follows Carl and his talking cat, Donut, as they navigate the Iron Tangle, a massive, deadly subway system. They must solve complex puzzles and survive dangerous AI-driven challenges while evading massive bounties placed on their heads by the galactic television network.",
+    ebook: "",
+    book: "",
   },
+  {
+    id: 4,
+    title: "The Gate Of The Feral Gods",
+    image: "/src/assets/book4.jpg",
+    imageCover: "/src/assets/book4-3d.png",
+    bookInfo: "Fourth book, floor 5",
+    introduction:
+      "Carl and Donut break the 5th floor's rules using chaotic alchemy and an ancient gate. Their actions destroy rival armies, sabotage alien sponsors, and spark a rebellion against the system.",
+    description:
+      "Dungeon Crawler Carl, Book 4: The Gate of the Feral Gods follows Carl, Princess Donut, and Katia on the deadly 5th floor of the dungeon. Forced into forced collaboration, all remaining players must conquer four massive castles scattered across land, sea, air, and sky within fifteen days to survive.",
+    ebook: "",
+    book: "",
+  },
+  // {
+  //   id: 5,
+  //   title: "The Bucher's Masquerade",
+  //   image: "/src/assets/book5.jpg",
+  //   bookInfo: "Fifth book, floor 6",
+  //   description: "",
+  //   ebook: "",
+  //   book: "",
+  // },
 ];
 
 export const connections = [
--- src/const/characters.js
+++ src/const/characters.js
@@ -2,8 +2,9 @@ export const characters = [
   {
     id: 0,
     name: "Donut",
-    img: "/public/character/donut-character2.png",
-    selection_img: "/public/character-selection/donut.png",
+    aliase: "Princess Donut, the Queen Anne Chonk, Best in Dungeon",
+    img: "/public/character/donut-character2",
+    selection_img: "/public/character-selection/donut",
     description:
       "Princess Donut is the fan-favorite, royal-acting tortoiseshell Persian cat from the hit LitRPG series Dungeon Crawler Carl by Matt Dinniman. She is sapient, speaks with a sultry yet pompous voice, and possesses a massive ego, constantly referring to herself as 'the queen' or leader of the team.",
     crawler_info: {
@@ -12,22 +13,13 @@ export const characters = [
       origin: "Seattle, WA",
       job: "Cat Lady",
     },
-    quotes: [
-      "You tried to kill Katia you fucking bitch!",
-      "DIE MOTHERFUCKERS DIE!!",
-    ],
-    physical_traits: {
-      age: "4",
-      height: "???",
-      weight: "???",
-      hair: "Black, Beige, and White",
-    },
   },
   {
     id: 1,
     name: "Carl",
-    img: "/public/character/carl.png",
-    selection_img: "/public/character-selection/carl.png",
+    aliase: "Royal Guardian of Princess Donut",
+    img: "/public/character/carl",
+    selection_img: "/public/character-selection/carl",
     description:
       "Carl is the titular protagonist of Dungeon Crawler Carl. A former coast guardsman, Carl and his ex-girlfriend's cat are now fan-favorite crawlers on the Syndicate gameshow, Dungeon Crawler World: Earth.",
     crawler_info: {
@@ -36,22 +28,13 @@ export const characters = [
       origin: "Seattle, WA",
       job: "Marine Technician",
     },
-    quotes: [
-      "asdYou tried to kill Katia you fucking bitch!",
-      "DIE MOTHERFUCKERS DIE!!",
-    ],
-    physical_traits: {
-      age: "27",
-      height: "6'3 (190.5cm)",
-      weight: "230 lbs (104.33 kg)",
-      hair: "Brown",
-    },
   },
   {
     id: 2,
     name: "Mordekai",
-    img: "/public/character/mordekai.png",
-    selection_img: "/public/character-selection/mordekai.png",
+    aliase: "",
+    img: "/public/character/mordekai",
+    selection_img: "/public/character-selection/mordekai",
     description:
       "Mordecai is Carl and Donut's Game Guide in the dungeon. He is an indentured employee of Borant Corporation, tasked with helping the corporation run crawls. Originally an avian race called a Skyfowl, Mordecai chose Changeling as his race when he was a crawler.",
     crawler_info: {
@@ -60,22 +43,13 @@ export const characters = [
       origin: "Skyfowl Planet",
       job: "Game Guide",
     },
-    quotes: [
-      "You tried to kill Katia you fucking bitch!",
-      "DIE MOTHERFUCKERS DIE!!",
-    ],
-    physical_traits: {
-      age: "???",
-      height: "???",
-      weight: "???",
-      hair: "???",
-    },
   },
   {
     id: 3,
     name: "Beatrice",
-    img: "/public/character/beatriz.png",
-    selection_img: "/public/character-selection/beatriz.png",
+    aliase: "Ms. Beatrice",
+    img: "/public/character/beatriz",
+    selection_img: "/public/character-selection/beatriz",
     description:
       "Beatrice begins as Carl's carefree ex-girlfriend and Donut's devoted owner, a symbol of the pre-dungeon normalcy shattered by the crawl, her absence haunting Carl amid the apocalypse. ",
     crawler_info: {
@@ -84,22 +58,13 @@ export const characters = [
       origin: "Seattle, WA",
       job: "Cat Show Person, MLM Boss Babe",
     },
-    quotes: [
-      "You don’t even like her, Carl. Why do you care?",
-      "DIE MOTHERFUCKERS DIE!!",
-    ],
-    physical_traits: {
-      age: "???",
-      height: "???",
-      weight: "???",
-      hair: "???",
-    },
   },
   {
     id: 4,
     name: "Mongo",
-    img: "/public/character/mongo.png",
-    selection_img: "/public/character-selection/mongo.png",
+    aliase: "",
+    img: "/public/character/mongo",
+    selection_img: "/public/character-selection/mongo",
     description:
       "Mongo is a highly intelligent, rapidly growing Velociraptor (specifically a Deinonychus-sized dinosaur) that becomes the beloved pet of Princess Donut",
     crawler_info: {
@@ -108,22 +73,13 @@ export const characters = [
       origin: "Second Floor",
       job: "Cat Show Person, MLM Boss Babe",
     },
-    quotes: [
-      "You don’t even like her, Carl. Why do you care?",
-      "DIE MOTHERFUCKERS DIE!!",
-    ],
-    physical_traits: {
-      age: "???",
-      height: "???",
-      weight: "???",
-      hair: "Pink",
-    },
   },
   {
     id: 5,
     name: "Imani",
-    img: "/public/character/imani.png",
-    selection_img: "/public/character-selection/imani.png",
+    aliase: "",
+    img: "/public/character/imani",
+    selection_img: "/public/character-selection/imani",
     description:
       "Imani is a member of the Meadow Lark party and guildmaster of Safehome Yolanda. She is one of Carl and Donut's most trusted allies in the Dungeon.",
     crawler_info: {
@@ -132,22 +88,13 @@ export const characters = [
       origin: "Wenatchee, WA",
       job: "Nurse",
     },
-    quotes: [
-      "You don’t even like her, Carl. Why do you care?",
-      "DIE MOTHERFUCKERS DIE!!",
-    ],
-    physical_traits: {
-      age: "approx. 20",
-      height: "5'2 (152.4cm)",
-      weight: "90 lbs.",
-      hair: "Brown",
-    },
   },
   {
     id: 6,
     name: "Brandon",
-    img: "/public/character/brandon.png",
-    selection_img: "/public/character-selection/brandon.png",
+    aliase: "",
+    img: "/public/character/brandon",
+    selection_img: "/public/character-selection/brandon",
     description:
       "Before the transformation, Brandon and his brother Chris Andrews were maintenance workers at the Meadow Lark Elder Care Facility in Wenatchee, WA.",
     crawler_info: {
@@ -156,21 +103,13 @@ export const characters = [
       origin: "Wenatchee, WA",
       job: "Maintenance",
     },
-    quotes: [
-      "We talked about it. All of us are going to go down. We’ll take our chances on the third floor. We don’t want to risk getting separated.",
-    ],
-    physical_traits: {
-      age: "???",
-      height: "???",
-      weight: "???",
-      hair: "Brown",
-    },
   },
   {
     id: 7,
     name: "Maggie My",
-    img: "/public/character/maggie.png",
-    selection_img: "/public/character-selection/maggie.png",
+    aliase: "",
+    img: "/public/character/maggie",
+    selection_img: "/public/character-selection/maggie",
     description:
       "Maggie My is a crawler from Earth and an ex-wife to Frank Q. After her daughter Yvette's death, she swears vengeance upon Carl and Donut and focuses her efforts on killing them rather than surviving.",
     crawler_info: {
@@ -178,20 +117,14 @@ export const characters = [
       crawler_id: "Maggie My",
       origin: "Seattle, WA",
       job: "Police Detective",
-    },
-    quotes: ["You don’t know anything. You didn’t get the whole story."],
-    physical_traits: {
-      age: "approx. 40",
-      height: "???",
-      weight: "???",
-      hair: "Brown",
     },
   },
   {
     id: 8,
     name: "Maggie My",
-    img: "/public/character/maggie.png",
-    selection_img: "/public/character-selection/maggie.png",
+    aliase: "",
+    img: "/public/character/a",
+    selection_img: "/public/character-selection/a",
     description:
       "Maggie My is a crawler from Earth and an ex-wife to Frank Q. After her daughter Yvette's death, she swears vengeance upon Carl and Donut and focuses her efforts on killing them rather than surviving.",
     crawler_info: {
@@ -199,13 +132,6 @@ export const characters = [
       crawler_id: "Maggie My",
       origin: "Seattle, WA",
       job: "Police Detective",
-    },
-    quotes: ["You don’t know anything. You didn’t get the whole story."],
-    physical_traits: {
-      age: "approx. 40",
-      height: "???",
-      weight: "???",
-      hair: "Brown",
     },
   },
 ];
